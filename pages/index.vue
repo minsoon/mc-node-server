bddf540c4d2fca815a8ac63a056d784ca99e6bcc
golfzon <mcsung@golfzon.com>
2019-01-14 14:10:01 +0900

    update dist

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     <div class="movie" v-if="movie">
       <img :src="movie.Poster">
       <h1>{{movie.Title}}</h1>
-      <h3>{{movie.Year}}</h3>
       <button @click="getRandomMovie">Get Random Movie</button>
     </div>
   </section>
