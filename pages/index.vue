43d846db331453f7727ce16d3a74a8b774540b96
golfzon <mcsung@golfzon.com>
2019-01-14 15:05:02 +0900

    update api test

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,27 @@
 <template>
   <section class="container">
-    {{ title }} 왜이래!
+    <div class="movie" v-if="movie">
+      <img :src="movie.Poster">
+      <h1>{{movie.Title}}</h1>
+      <h3>{{movie.Year}}</h3>
+      <button @click="getRandomMovie">Get Random Movie</button>
+    </div>
   </section>
 </template>
 
 <script>
   import axios from 'axios'
   export default {
-    data () {
-      return {
-        title: 'mc-nuxt-server'
+    asyncData({ params }) {
+      return axios.get('http://localhost:3000/api/random-movie').then(res => {
+          return { movie: res.data }
+        })
+    },
+    methods: {
+      getRandomMovie() {
+        axios.get('api/random-movie').then(res => {
+          this.movie = res.data
+      })
       }
     }
   }
